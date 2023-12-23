036674063e77ab959aa16d2be303b9588c4e56ed
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 20:35:06 +0300

    Adding home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
-import Novel from "./novel/[id]/page";
-
+import { Novels } from "../components/Novels";
 export default function Home() {
-  return <main className="max-w-5xl bg-red h-10">{/* <Novel /> */}</main>;
+  return (
+    <div className=" max-w-5xl mx-auto ">
+      {" "}
+      <Novels />
+    </div>
+  );
 }
