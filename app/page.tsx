83b2c72b06bd5bf50f783da6fa73e726235bde74
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 20:40:26 +0300

    final commit -m

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Novels } from "../components/Novels";
 export default function Home() {
   return (
     <div className=" max-w-5xl mx-auto ">
-      {" "}
       <Novels />
     </div>
   );
