df1822bfd709fad633cfd6c71b90fa79ac9a4cd5
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 17:38:07 +0300

    update novel function

--- graphql/mutation.ts
+++ graphql/mutation.ts
@@ -26,3 +26,13 @@ export const DELETE_NOVEL = gql`
     }
   }
 `;
+
+export const UPDATE_NOVEL = gql`
+  mutation UpdateNovel($id: ID!, $title: String, $image: String) {
+    updateNovel(id: $id, title: $title, image: $image) {
+      id
+      image
+      title
+    }
+  }
+`;
