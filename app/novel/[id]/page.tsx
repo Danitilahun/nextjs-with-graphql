0004f3fe255a66612da8b48d3c22daa5616deb59
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-23 20:23:44 +0300

    Adding indivitual nove page

--- app/novel/[id]/page.tsx
+++ app/novel/[id]/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { ADD_AUTHOR, DELETE_AUTHOR, UPDATE_NOVEL } from "@/graphql/mutation";
 import { GET_NOVEL } from "@/graphql/queries";
 import { INovel } from "@/typings";
@@ -22,6 +21,21 @@ const Novel = ({ params: { id } }: Props) => {
   });
 
   const novel: INovel = data?.novel;
+
+  const handleAddAuthor = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (name === "") return alert("Please enter author name");
+    addAuthor({ variables: { novelId: id, name } });
+    setName("");
+  };
+
+  const handleUpdateNovel = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (title === "" || url === "") return alert("Please enter fields");
+    updateNovel({ variables: { id: id, title: title, image: url } });
+    setTitle("");
+    setUrl("");
+  };
 
   const [addAuthor] = useMutation(ADD_AUTHOR, {
     variables: { novelId: id, name },
@@ -50,7 +64,80 @@ const Novel = ({ params: { id } }: Props) => {
       </p>
     );
 
-  return <article className="max-w-5xl bg-red"></article>;
+  return (
+    <article className="max-w-5xl mx-auto text-white">
+      <section className="flex gap-2 ">
+        {novel.image && (
+          <img height={200} width={200} src={novel.image} alt="" />
+        )}
+
+        <div className="p-2 flex flex-col">
+          <h1 className="text-4xl ">Title : {novel.title}</h1>
+
+          <div className="flex gap-2">
+            {novel?.authors?.map((author) => (
+              <div key={author.id} className="flex items-center gap-2">
+                <h2 className="font-bold">{author?.name}</h2>
+                <div
+                  onClick={() =>
+                    deleteAuthor({
+                      variables: {
+                        id: author.id,
+                      },
+                    })
+                  }
+                  color="yellow"
+                />
+              </div>
+            ))}
+          </div>
+          <p className="text-slate-400 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+            cum nam sed voluptates sunt aliquid nemo maxime itaque tempora,
+            autem alias nostrum molestiae deserunt earum animi numquam
+            reprehenderit laboriosam libero? Quas, atque totam vero nostrum
+            dolore, nihil autem neque architecto deserunt illo itaque, ab quae
+            ipsam corrupti ipsum quaerat? Sed hic ipsum excepturi earum minus
+            consectetur soluta totam temporibus libero.
+          </p>
+          {/* add author form */}
+          <form onSubmit={handleAddAuthor} className="mt-5 space-x-2">
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              placeholder="Enter Author"
+              className="bg-transparent border p-2 mx-2"
+            />
+            <button
+              disabled={!name}
+              className="border p-2 rounded-lg disabled:text-gray-500 disabled:cursor-not-allowed"
+            >
+              Add Author
+            </button>
+          </form>
+        </div>
+      </section>
+      {/* update form */}
+      <form onSubmit={handleUpdateNovel} className="flex gap-2 ">
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Enter new title"
+          className="bg-transparent border text-white p-2 rounded-lg"
+        />
+        <input
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          type="text"
+          placeholder="new url"
+          className="bg-transparent border text-white p-2 rounded-lg"
+        />
+        <button className="bg-yellow-500 rounded-lg p-2">Update</button>
+      </form>
+    </article>
+  );
 };
 
 export default Novel;
